6c3bae20312746cbf80ed991e38c2d4bcc0b64e8
dermatologist <github@gulfdoctor.net>
2018-09-30 08:03:45 -0400

    Before channel creation

--- src/main/interface.go
+++ src/main/interface.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/jroimartin/gocui"
+	"flag"
+	"io/ioutil"
 )
 
 func nextView(g *gocui.Gui, v *gocui.View) error {
@@ -64,4 +66,69 @@ func getLine(g *gocui.Gui, v *gocui.View) error {
 
 func quit(g *gocui.Gui, v *gocui.View) error {
 	return gocui.ErrQuit
+}
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("title", -1, -1, maxX, 3); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Highlight = true
+		v.SelBgColor = gocui.ColorRed
+		v.SelFgColor = gocui.ColorBlack
+		fmt.Fprintln(v, "OSCAR eForm Export Tool Helper by Bell Eapen")
+	}
+	if v, err := g.SetView("side", -1, 4, 30, maxY); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Highlight = true
+		v.SelBgColor = gocui.ColorGreen
+		v.SelFgColor = gocui.ColorBlack
+		fmt.Fprintln(v, "Column 1")
+		fmt.Fprintln(v, "Column 2")
+		fmt.Fprintln(v, "Column 3")
+		fmt.Fprint(v, "\rWill be")
+		fmt.Fprint(v, "deleted\rColumn 4\nColumn 5")
+	}
+	if v, err := g.SetView("main", 30, 4, maxX, maxY); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		filePtr := flag.String("word", "test.csv", "The csv file to process")
+		b, err := ioutil.ReadFile(*filePtr)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Fprintf(v, "%s", b)
+		v.Editable = true
+		v.Wrap = true
+		if _, err := g.SetCurrentView("main"); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func keybindings(g *gocui.Gui) error {
+	if err := g.SetKeybinding("side", gocui.KeyCtrlSpace, gocui.ModNone, nextView); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("main", gocui.KeyCtrlSpace, gocui.ModNone, nextView); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("side", gocui.KeyArrowDown, gocui.ModNone, cursorDown); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("side", gocui.KeyArrowUp, gocui.ModNone, cursorUp); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		return err
+	}
+	if err := g.SetKeybinding("side", gocui.KeyEnter, gocui.ModNone, getLine); err != nil {
+		return err
+	}
+	return nil
 }
