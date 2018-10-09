36a2ac7200c5eb5d022a9f3aa335a3832d61fad3
Bell Eapen <github@gulfdoctor.net>
2018-10-09 10:56:49 -0400

    SQL updated with dateFrom and dateTo

--- src/main/mysql-connect.go
+++ src/main/mysql-connect.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"database/sql"
+	"flag"
 	"fmt"
 	"github.com/go-sql-driver/mysql"
 	"golang.org/x/crypto/ssh"
 	"golang.org/x/crypto/ssh/agent"
-	"log"
 	"net"
 	"os"
 	"strconv"
-	"time"
 )
 
 type ViaSSHDialer struct {
@@ -22,6 +21,16 @@ func (self *ViaSSHDialer) Dial(addr string) (net.Conn, error) {
 }
 
 func mysqlConnect() (*sql.Rows, error) {
+	sshHost := flag.String("host", "example.com", "The SSH host")
+	sshPort := flag.Int("port", 22, "The port number")
+	sshUser := flag.String("sshuser", "ssh-user", "ssh user")
+	sshPass := flag.String("sshpass", "ssh-pass", "SSH Password")
+	dbUser := flag.String("dbuser", "dbuser", "The db user")
+	dbPass := flag.String("dbpass", "dbpass", "The db password")
+	dbHost := flag.String("dbhost", "localhost:3306", "The db host")
+	dbName := flag.String("dbname", "oscar", "The database name")
+	fid := flag.Int("fid", 1, "The eform ID")
+
 	var agentClient agent.Agent
 	// Establish a connection to the local ssh-agent
 	if conn, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK")); err == nil {
@@ -35,10 +44,6 @@ func mysqlConnect() (*sql.Rows, error) {
 	sshConfig := &ssh.ClientConfig{
 		User: *sshUser,
 		Auth: []ssh.AuthMethod{},
-		HostKeyCallback: func(hostname string, remote net.Addr, key ssh.PublicKey) error {
-			return nil
-		},
-		Timeout: time.Second * 5,
 	}
 
 	// When the agentClient connection succeeded, add them as AuthMethod
@@ -64,7 +69,10 @@ func mysqlConnect() (*sql.Rows, error) {
 
 			fmt.Printf("Successfully connected to the db\n")
 
-			if rows, err := db.Query("SELECT id, fdid, fid, demographic_no, var_name, var_value FROM eform_values WHERE fid = " + strconv.Itoa(*fid) + ";"); err == nil {
+			if rows, err := db.Query("SELECT id, fdid, fid, demographic_no, var_name, 
+				var_value FROM eform_values WHERE fid = " + strconv.Itoa(*fid) + " AND fdid IN
+				 (SELECT fdid FROM eform_data WHERE fid = " + strconv.Itoa(*fid) + " AND form_date >= " 
+				 + *dateFrom + " AND form_date <= " + *dateTo + " );"); err == nil {
 				//for rows.Next() {
 				//	var id int64
 				//	var name string
@@ -86,9 +94,6 @@ func mysqlConnect() (*sql.Rows, error) {
 			return nil, err
 		}
 
-	} else {
-		log.Panicln(err)
-		return nil, err
 	}
 	return nil, nil
 }
